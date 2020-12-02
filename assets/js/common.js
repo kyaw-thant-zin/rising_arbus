c1ed360b958ac1d85a4a6f27c2c418e687236b17
Kyaw Thant Zin <kyaw-thant-zin@users.noreply.github.com>
2020-12-02 13:30:28 +0630

    Update common.js

---
+++ assets/js/common.js
@@ -0,0 +1,5 @@
+$('.menu-trigger,.nav-list li a').on('click', function () {
+    $('.menu-trigger').toggleClass('active');
+    $('body').toggleClass('noscroll');
+    $('.nav-list').toggleClass('toggle');
+});
